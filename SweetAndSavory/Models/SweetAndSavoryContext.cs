74bd944ad34879fb014df5e68977d165ad90da64
Josephwjack <joseph.w.jack@gmail.com>
2022-08-12 09:23:54 -0700

    add View folders and layout file

--- SweetAndSavory/Models/SweetAndSavoryContext.cs
+++ SweetAndSavory/Models/SweetAndSavoryContext.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SweetAndSavory.Models;
 
 namespace SweetAndSavory.Models
 {
   public class SweetAndSavoryContext : IdentityDbContext<ApplicationUser>
   {
     
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<FlavorTreat> FlavorTreat { get; set; }
 
     public SweetAndSavoryContext(DbContextOptions options) : base(options) { }
 
